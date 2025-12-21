346c265f1b99b9bf10e6f6ab694fb52c211c39bb
Damim-03 <imadeddinekir95@gmail.com>
2025-12-21 16:40:32 +0100

    ===

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const express_2 = require("@clerk/express");
 const env_1 = require("./config/env");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, express_2.clerkMiddleware)());
 app.get("/api/health", (_req, res) => {
     res.send("The server is healthy.");
 });
--- Backend/dist/config/env.js
+++ Backend/dist/config/env.js
@@ -3,10 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VITE_CLERK_PUBLISHABLE_KEY = exports.MONGO_URI = exports.NODE_ENV = exports.PORT = void 0;
+exports.INNGEST_SIGNING_KEY = exports.CLOUDINARY_CLOUD_NAME = exports.CLOUDINARY_API_SECRET = exports.CLOUDINARY_API_KEY = exports.CLERK_SECRET_KEY = exports.CLERK_PUBLISHABLE_KEY = exports.MONGO_URI = exports.NODE_ENV = exports.PORT = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.PORT = process.env.PORT;
 exports.NODE_ENV = process.env.NODE_ENV;
 exports.MONGO_URI = process.env.MONGO_URI;
-exports.VITE_CLERK_PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
+exports.CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
+exports.CLERK_SECRET_KEY = process.env.CLERK_SECRET_KEY;
+exports.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+exports.CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+exports.CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+exports.INNGEST_SIGNING_KEY = process.env.INNGEST_SIGNING_KEY;
