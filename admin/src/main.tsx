60b794d13e5938300948442d3cb3ad7ef2fb6d3b
Damim-03 <imadeddinekir95@gmail.com>
2025-12-31 20:16:10 +0100

    fix

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
-import * as Sentry from "@sentry/react";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -16,13 +15,6 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const queryClient = new QueryClient();
-
-Sentry.init({
-  dsn: "https://88c8585b2fd07d0ac4e64bb1b68eaf58@o4510152714485760.ingest.de.sentry.io/4510630520815696",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
