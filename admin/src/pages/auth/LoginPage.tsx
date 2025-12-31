d7d64018f7a0b23c0bf9d6f75e807397cc8150de
Damim-03 <imadeddinekir95@gmail.com>
2025-12-31 19:02:32 +0100

    fix issues

--- admin/src/pages/auth/LoginPage.tsx
+++ admin/src/pages/auth/LoginPage.tsx
@@ -1,10 +1,10 @@
-import { SignIn } from "@clerk/clerk-react"
+import { SignIn } from "@clerk/clerk-react";
 
-
-const LoginPage = () => {
+function LoginPage() {
   return (
-    <><div>LoginPage</div><SignIn /></>
-  )
+    <div className="h-screen hero">
+      <SignIn />
+    </div>
+  );
 }
-
-export default LoginPage
+export default LoginPage;
